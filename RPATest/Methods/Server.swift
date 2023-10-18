0fa45abd6db63103cbaa443564ae003526a77f60
Jusung Lee <js100810081@gmail.com>
2023-10-18 09:32:20 +0900

    Change NH Server URL

--- RPATest/Methods/Server.swift
+++ RPATest/Methods/Server.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum ServerURL: String {
     case DEV = "http://34.121.50.23:8000"
-    case NH = "http://34.125.128.74/"
+    case NH = "http://34.125.128.74:5000/"
     case PROD = "http://api.kingbuserp.link"
 }
 
