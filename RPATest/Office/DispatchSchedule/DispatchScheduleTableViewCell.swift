02184a0182b3b77db73b871070c5b21ff474faae
Jusung Lee <js100810081@gmail.com>
2023-11-09 17:47:19 +0900

    배차표 추가

---
+++ RPATest/Office/DispatchSchedule/DispatchScheduleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DispatchScheduleTableViewCell.swift
+//  RPATest
+//
+//  Created by Awesomepia on 11/9/23.
+//
+
+import UIKit
+
+class DispatchScheduleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RPATest/Office/DispatchSchedule/DispatchScheduleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DispatchScheduleViewController.swift
+//  RPATest
+//
+//  Created by Awesomepia on 11/9/23.
+//
+
+import UIKit
+
+class DispatchScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
